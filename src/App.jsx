7d70189080b1a7082b7112eaaf880c79b9e78e62
manojp675 <mp4752530@gmail.com>
2025-12-30 16:47:41 +0530

    fix the issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import Navbar from "./Components/Navbar";
-import NewsBoard from "./Components/NewsBoard";
+import Navbar from "./components/Navbar";
+import Newsboard from "./Components/Newsboard";
+
 
 const App = () => {
   const [category, setCategory] = useState("general");
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <>
       <Navbar setCategory={setCategory} />
-      <NewsBoard category={category} />
+      <Newsboard category={category} />
     </>
   );
 };
